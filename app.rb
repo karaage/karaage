ea267d815f14add35950631014df62529eec4e35
koduki <pascalm3@gmail.com>
2010-11-27 17:20:26 +0900

    add atnd feed and link.

--- app.rb
+++ app.rb
@@ -1,9 +1,10 @@
 require 'sinatra'
 require 'haml'
 require 'erubis'
+require File.expand_path('../lib/karaage_atnd_feed.rb',  __FILE__)
 
 
 get '/' do
-   events = KaraageAtndFeed.new.get_events
+	@events = KaraageAtndFeed.new.get_events
 	erubis :index
 end
