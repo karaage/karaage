522726ad650e78dd174a02debc6ca1117b627053
koduki <pascalm3@gmail.com>
2010-11-28 15:47:59 +0900

    apply bundler auto require.

--- app.rb
+++ app.rb
@@ -1,10 +1,15 @@
-require 'sinatra'
-require 'haml'
-require 'erubis'
+#require 'sinatra'
+#require 'haml'
+#require 'erubis'
 require File.expand_path('../lib/karaage_atnd_feed.rb',  __FILE__)
 
+class Application < Sinatra::Base
+   def initialize(app)
+      super(app)
+   end
 
-get '/' do
-	@events = KaraageAtndFeed.new.get_events
-	erubis :index
+   get '/' do
+	   @events = KaraageAtndFeed.new.get_events
+	   erubis :index
+   end
 end
--- config.ru
+++ config.ru
@@ -1,9 +1,13 @@
 $:.unshift File.expand_path('./', File.dirname(__FILE__))
 
+require 'bundler'
+Bundler.require
+
 require 'app'
 
 set :run, false
 set :environment, ENV['RACK_ENV']
 
+use Application
 run Sinatra::Application
 
