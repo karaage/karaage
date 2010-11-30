d9fb18e0cb900f3e9b4fbee28f63f1f28e802bc2
Akihito Tsukamoto <tsuka05@gmail.com>
2010-11-30 15:42:47 +0900

    add static file setting.

--- app.rb
+++ app.rb
@@ -3,6 +3,7 @@ require File.expand_path('../lib/karaage_atnd_feed.rb',  __FILE__)
 class Application < Sinatra::Base
    def initialize(app)
       super(app)
+      set :public, File.dirname(__FILE__) + '/public'
    end
 
    get '/' do
